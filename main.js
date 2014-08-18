5eae5f1a3cb7d1204e6de573bd71e348a2da6397
Tony Xiao <tony@happybits.co>
2014-08-18 00:01:48 -0700

    Second iteration which uses hard-coded cookie value as well as internal TestFlight restAPI to obtain installation URL

--- main.js
+++ main.js
@@ -1,45 +1,25 @@
-var request = require('request').defaults({jar: true});
-var jsdom = require('jsdom');
-var jquery = require('jquery');
+var tfCookie = '93d98b24ff70c226de78a6809e3b0f05';
+var tfAppId = 1267682; // Marco Polo Dev
+
 var express = require('express');
+var request = require('request').defaults({
+	jar: true,
+	json: true,
+	headers: {
+		'Cookie': 'tfapp=' + tfCookie + ';'
+	}
+});
 var app = express();
 
 
-
-
-app.get('/', function(req, response) {
-	request.get('https://testflightapp.com/login/', function(err, res, body) {
-	  	jsdom.env(body, function(err, window) {
-			var $ = jquery(window);
-			var csrf = $('input[name=csrfmiddlewaretoken]').val();
-			console.log('csrf ', csrf);
-
-			request.post('https://testflightapp.com/login/', {
-				headers: {
-					'Referer': 'https://testflightapp.com/login/'
-				},
-				form: {
-					'csrfmiddlewaretoken': csrf,
-					'username': 'tony@happybits.co',
-					'password': '******'
-				}
-			}, function(err, res, body) {
-				console.log(res.statusCode);
-				// console.log(res.headers);
-				var url = 'https://www.testflightapp.com/install/65d3029c8f088f160f35d40b89c3612e-MTI1NTU5NjM/';
-				url = 'https://www.testflightapp.com/dashboard/ipa/65d3029c8f088f160f35d40b89c3612e-MTI1NTU5NjM/'
-				request.get(url, {
-					headers: {
-						'User-Agent': 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7'
-					},
-					followRedirect: false
-				}, function(err, res, body) {
-					console.log(res.statusCode);
-					console.log(res.headers);
-					response.redirect(res.headers['location']);
-				});
-			});
-
+app.get('/latest', function(req, response) {
+	// First find ID of the latest build
+	request.get('https://testflightapp.com/m/api/apps/' + tfAppId, function(err, res, body) {
+		var buildID = body['list'][0]['id'];
+		// Then retrieve installation URL of the latest build
+		request.get('https://testflightapp.com/m/api/apps/' + tfAppId + '/' + buildID, function(err, res, body) {
+			console.log('will redirect to ' + body['install_url']);
+			response.redirect(body['install_url']);
 		});
 	});
 });
